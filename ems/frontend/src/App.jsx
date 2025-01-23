83c8b714b64aa8c994c4658c36460ca7e91685d0
vin <krishanth.cse@gmail.com>
2025-01-23 17:05:14 +0530

    First commit

---
+++ ems/frontend/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <h1 class="text-3xl font-bold text-blue-500 text-center">Hello, Tailwind!</h1>
+    </div>
+  )
+}
+
+export default App
